170f223c2bdfce8dcd9586f070955b29a314b9ac
noobscode <nobbassen@gmail.com>
2018-01-24 22:22:50 +0100

    permission work

--- setup.py
+++ setup.py
@@ -60,12 +60,12 @@ def install():
     subprocess.Popen("chmod +x /usr/bin/kalelvpn", shell=True).wait()
 
     # Write permission to run
-    subprocess.call(['chmod', '+x', '/opt/KalEl/run.py'])
-    subprocess.call(['chmod', '+x', '/opt/KalEl/module/tor/tor.py'])
-    subprocess.call(['chmod', '+x', '/opt/KalEl/module/ettercap/spoof.py'])
-    subprocess.call(['chmod', '+x', '/opt/KalEl/module/harvester/prep.py'])
-    subprocess.call(['chmod', '+x', '/opt/KalEl/module/harvester/engine.py'])
-    subprocess.call(['chmod', '+x', '/opt/KalEl/module/spoofmail/spoofmail.py'])
+    subprocess.Popen(['chmod', '+x', '/opt/KalEl/run.py'])
+    subprocess.Popen(['chmod', '+x', '/opt/KalEl/module/tor/tor.py'])
+    subprocess.Popen(['chmod', '+x', '/opt/KalEl/module/ettercap/spoof.py'])
+    subprocess.Popen(['chmod', '+x', '/opt/KalEl/module/harvester/prep.py'])
+    subprocess.Popen(['chmod', '+x', '/opt/KalEl/module/harvester/engine.py'])
+    subprocess.Popen(['chmod', '+x', '/opt/KalEl/module/spoofmail/spoofmail.py'])
 
     # Check if config files is present, if they are we will remove them
     if os.path.isfile("/opt/KalEl/src/setupOK"):
