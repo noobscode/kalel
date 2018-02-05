f57913654ff015bafe5567acec2904554990deca
noobscode <nobbassen@gmail.com>
2018-02-05 06:17:36 +0100

    fixed print function on hashbuster

--- module/cracking/hashbuster/hashbuster.py
+++ module/cracking/hashbuster/hashbuster.py
@@ -25,26 +25,26 @@ def omega():
     find = html.read()
     match = search (r'<b>[^<]*</b><br/><br/>', find)
     if match:
-        print "\n%s %s : " % (good, hashvalue), match.group().split('<b>')[1][:-14]
+        print("\n%s %s : ") % (good, hashvalue), match.group().split('<b>')[1][:-14]
     else:
         if not args.path:
-            print "%s Sorry this hash is not present in our database." % bad
+            print("%s Sorry this hash is not present in our database.") % bad
 
 def Lambda():
     html = urlopen("http://md5decrypt.net/Api/api.php?hash=%s&hash_type=sha256&email=deanna_abshire@proxymail.eu&code=1152464b80a61728" % hashvalue)
     find = html.read()
     if len(find) > 0:
-        print "\n%s %s : " % (good, hashvalue), find
+        print("\n%s %s : ") % (good, hashvalue), find
     else:
         if not args.path:
-            print "%s Sorry this hash is not present in our database." % bad
+            print("%s Sorry this hash is not present in our database.") % bad
 def beta():
     data = urlencode({"auth":"8272hgt", "hash":hashvalue, "string":"","Submit":"Submit"})
     html = urlopen("http://hashcrack.com/index.php" , data)
     find = html.read()
     match = search (r'<span class=hervorheb2>[^<]*</span></div></TD>', find)
     if match:
-        print "\n%s %s : " % (good, hashvalue), match.group().split('hervorheb2>')[1][:-18]
+        print("\n%s %s : ") % (good, hashvalue), match.group().split('hervorheb2>')[1][:-18]
     else:
         omega()
 
@@ -58,38 +58,38 @@ def intro():
 def crack(hashvalue):
     if len(hashvalue) == 32:
         if not args.path:
-            print "%s Hash function : MD5" % info
+            print("%s Hash function : MD5") % info
         data = urlencode({"hash":hashvalue,"submit":"Decrypt It!"})
         html = urlopen("http://md5decryption.com", data)
         find = html.read()
         match = search(r"Decrypted Text: </b>[^<]*</font>", find)
         if match:
-            print "\n%s %s : " % (good, hashvalue), match.group().split('b>')[1][:-7]
+            print("\n%s %s : ") % (good, hashvalue), match.group().split('b>')[1][:-7]
         else:
             data = urlencode({"md5":hashvalue,"x":"21","y":"8"})
             html = urlopen("http://md5.my-addr.com/md5_decrypt-md5_cracker_online/md5_decoder_tool.php", data)
             find = html.read()
             match = search (r"<span class='middle_title'>Hashed string</span>: [^<]*</div>", find)
             if match:
-                print "\n%s %s :" % (good, hashvalue), match.group().split('span')[2][3:-6]
+                print("\n%s %s :") % (good, hashvalue), match.group().split('span')[2][3:-6]
             else:
                 url = "http://www.nitrxgen.net/md5db/" + hashvalue
                 purl = urlopen(url).read()
                 if len(purl) > 0:
-                    print "\n%s %s :" % (good, hashvalue), purl
+                    print("\n%s %s :") % (good, hashvalue), purl
                 else:
                     if not args.path:
-                        print "%s Sorry this hash is not present in our database." % bad
+                        print("%s Sorry this hash is not present in our database.") % bad
     elif len(hashvalue) == 40:
         if not args.path:
-            print "%s Hash function : SHA1" % info
+            print("%s Hash function : SHA1") % info
         beta()
     elif len(hashvalue) == 64:
         if not args.path:
-            print "%s Hash function : SHA-256" % info
+            print("%s Hash function : SHA-256") % info
         Lambda()
     else:
-        print "%s This hash is not supported.\n" % bad
+        print("%s This hash is not supported.\n") % bad
         raw_input(green + 'Press [ENTER] to go back to the menu!' + end)
 
 if args.path:
@@ -103,3 +103,4 @@ else:
     intro()
     hashvalue = raw_input('%s Enter your hash: ' % que).lower()
     crack(hashvalue)
+    raw_input(green + 'Press [ENTER] to go back to the menu!' + end)
